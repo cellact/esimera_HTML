1b45eec86bcf632c6d54c9fbea027abd68c79b56
YogatBear <yoav19993@gmail.com>
2025-04-28 15:53:32 +0200

    v1.0.4 - support for new json message format

---
+++ download_assets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download Material Icons font
+curl -L "https://fonts.googleapis.com/icon?family=Material+Icons" -o assets/icons/material-icons.css
+curl -L "https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2" -o assets/icons/MaterialIcons-Regular.woff2
+
+# Download Material Symbols Outlined font
+curl -L "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" -o assets/icons/material-symbols.css
+curl -L "https://fonts.gstatic.com/s/materialsymbolsoutlined/v146/kJF1BvYX7BgnkSrUwT8OhrdQw4oELdPIeeII9v6oDMzByHX9rA6RzaxHMPdY43zj-jCxv3fzvRNU22ZXGJpEpjC_1n-q_4MrImHCIJIZrDCvHOejbd5zrDAt.woff2" -o assets/icons/MaterialSymbolsOutlined-Regular.woff2
+
+# Download SF Pro Display font
+curl -L "https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@300;400;500;600&display=swap" -o assets/fonts/sf-pro-display.css
+
+# Download SF Pro Text font
+curl -L "https://fonts.googleapis.com/css2?family=SF+Pro+Text:wght@400;500;600&display=swap" -o assets/fonts/sf-pro-text.css 
